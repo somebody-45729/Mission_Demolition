a1d915595fd31421cbc4231d35237b6eb0d5063a
somebody-45729 <90016656+somebody-45729@users.noreply.github.com>
2022-02-16 14:29:13 -0600

    update

---
+++ Mission Demolition/Assets/Scripts/RigidbodySleep.cs
@@ -0,0 +1,19 @@
+// Description put rigidbody to sleep
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidbodySleep : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        if (rb != null) rb.Sleep();
+
+    }
+}
